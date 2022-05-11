3376cde06cca4e7b27d1bbb3e444ab76a320a1cf
unknown <tsanchit92@gmail.com>
2022-05-11 20:38:21 +0530

    payment-done

--- payment-micro/src/main/java/payment/model/Repo.java
+++
@@ -1,8 +0,0 @@
-package payment.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface Repo extends JpaRepository<UserName, Integer> {
-
-}
